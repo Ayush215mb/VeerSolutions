022ba8b75fd816fa67af7298e6f980a445aa07ff
Ayush Yadav <sidharth197502@gmail.com>
2024-08-07 17:14:16 +0530

    added email link in career.jsx

--- src/Components/Careers/CareerONE.jsx
+++ src/Components/Careers/CareerONE.jsx
@@ -15,33 +15,36 @@ const CareerONE = () => {
           className="max-w-64 md:max-w-lg"
         />
         <div className="flex flex-col gap-5 md:gap-10">
-          <h1 className="md:text-5xl md:font-bold font-semibold text-sm">
+          <h1 className="md:text-5xl md:font-bold font-semibold text-xl">
             Join Veer Solutions
           </h1>
           <div className="flex flex-col gap-2 md:gap-7 items-left justify-center">
-            <p className="flex md:items-center items-start gap-2  md:gap-4  text-sm md:text-3xl">
+            <p className="flex md:items-center items-start gap-2  md:gap-4  text-md md:text-3xl">
               <AiOutlineCaretRight className=" text-sm sm:text-md text-red-500 md:text-xl" />
               Flexible Work Schedule
             </p>
-            <p className="flex  md:items-center items-start gap-2 md:gap-4 text-sm sm:text-md  md:text-3xl">
+            <p className="flex  md:items-center items-start gap-2 md:gap-4 text-md sm:text-md  md:text-3xl">
               <AiOutlineCaretRight className=" text-md text-red-500 md:text-xl" />
               Competitive Salary
             </p>
-            <p className="flex  md:items-center items-start gap-2  md:gap-4  text-sm sm:text-md md:text-3xl">
+            <p className="flex  md:items-center items-start gap-2  md:gap-4  text-md sm:text-md md:text-3xl">
               <AiOutlineCaretRight className=" text-md text-red-500 md:text-xl" />
               Teambuildings & Events
             </p>
-            <p className="flex  md:items-center items-start gap-2  md:gap-4  text-sm sm:text-md  md:text-3xl">
+            <p className="flex  md:items-center items-start gap-2  md:gap-4  text-md sm:text-md  md:text-3xl">
               <AiOutlineCaretRight className=" text-md text-red-500 md:text-xl" />
               Annual Bonus
             </p>
           </div>
         </div>
       </div>
-      <div>
+      <div className="flex flex-col items-center gap-5">
         <h3 className="text-md md:text-3xl mt-3 mx-7 md:mt-5">
           Mail Us your resume and we will get back to you
         </h3>
+        <a href="mailto:sidharth197502@gmail.com?subject=Hello there&body=This is a predefined email body&cc=someone@example.com,bcc=someoneelse@example.com">
+          SEND MAIL
+        </a>
       </div>
     </div>
   );
