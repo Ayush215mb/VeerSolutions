edca12b37c6fdbf3efddfcb5096d43aa332ddad9
Ayush Yadav <sidharth197502@gmail.com>
2024-08-09 21:34:12 +0530

    added framer motion in a div of homepage

--- src/Components/Homepage/HeaderOne.jsx
+++ src/Components/Homepage/HeaderOne.jsx
@@ -1,20 +1,53 @@
 import Lottie from "lottie-react";
-import { useRef } from "react";
+import { useRef, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import ComputerAnime from "./../../assets/computer.json";
 
+import { motion } from "framer-motion";
+import { fadeIn } from "../../Variants";
+
 const HeaderOne = () => {
   const AniRef = useRef();
+  const [isVisible, setIsVisible] = useState(false);
+
+  const fadeInUpAnime = {
+    hidden: {
+      opacity: 0,
+      y: 100,
+    },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        staggerChildren: 0.3,
+        duration: 1.5,
+      },
+    },
+  };
+
+  useEffect(() => {
+    setIsVisible(true);
+  }, []);
 
   return (
     <div className="text-white flex flex-col-reverse  items-center md:flex-row mt-5  md:m-28 gap-6 md:gap-12 mb-5  ">
-      <div className="flex flex-col items-center md:items-start gap-6 md:gap-10 px-3 py-4 md:p-10">
-        <h1 className="sm:text-xl text-xl md:text-4xl font-semibold max-w-2xl text-wrap flex justify-center items-center ">
+      <motion.div
+        variants={fadeIn("right", 0, 2)}
+        initial="hidden"
+        whileInView={"show"}
+        viewport={{ once: true, amount: 0.7 }}
+        className="flex flex-col items-center md:items-start gap-6 md:gap-10 px-3 py-4 md:p-10"
+      >
+        <h1
+          className={` sm:text-xl text-xl md:text-4xl font-semibold max-w-2xl text-wrap flex justify-center items-center  `}
+        >
           Build your digital solutions with expert help
         </h1>
 
-        <p className="text-wrap flex justify-center max-w-80 md:max-w-xl items-center  text-lg sm:text-sm md:text-xl">
+        <p
+          className={`text-wrap flex justify-center max-w-80 md:max-w-xl items-center  text-lg sm:text-sm md:text-xl   `}
+        >
           Share your challenge with our team, who will work with you to deliver
           a revolutionary digital product.
         </p>
@@ -22,7 +55,7 @@ const HeaderOne = () => {
         <button className=" md:px-4 md:py-2 py-2 px-3  leading-snug text-white transition-colors bg-blue-600 rounded-md cursor-pointer hover:bg-blue-700 max-w-48  lg:block">
           <Link to="/Contact-Us"> Get in touch</Link>
         </button>
-      </div>
+      </motion.div>
 
       <Lottie
         //       // onComplete={() => {
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,44 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      extend: {
+        keyframes: {
+          fadeIn: {
+            "0%": { opacity: "0" },
+            "100%": { opacity: "1" },
+          },
+          slideIn: {
+            "0%": { transform: "translateX(-100%)" },
+            "100%": { transform: "translateX(0)" },
+          },
+          slideInLeft: {
+            "0%": { transform: "translateX(-100%)", opacity: "0" },
+            "100%": { transform: "translateX(0)", opacity: "1" },
+          },
+          slideInRight: {
+            "0%": { transform: "translateX(100%)", opacity: "0" },
+            "100%": { transform: "translateX(0)", opacity: "1" },
+          },
+          slideInTop: {
+            "0%": { transform: "translateY(-100%)", opacity: "0" },
+            "100%": { transform: "translateY(0)", opacity: "1" },
+          },
+          scaleUp: {
+            "0%": { transform: "scale(0.5)" },
+            "100%": { transform: "scale(1)" },
+          },
+        },
+        animation: {
+          fadeIn: "fadeIn 1s ease-in-out",
+          slideIn: "slideIn 1s ease-in-out",
+          slideInLeft: "slideInLeft 1s ease-out",
+          slideInRight: "slideInRight 1s ease-out",
+          slideInTop: "slideInTop 1s ease-out",
+          scaleUp: "scaleUp 1s ease-in-out",
+        },
+      },
+    },
   },
   plugins: [],
 };
