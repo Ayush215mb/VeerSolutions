92ce870501df327f0da93acdf360f0c986ef2b2a
Ayush Yadav <sidharth197502@gmail.com>
2024-08-30 13:43:09 +0530

    removed my portfolio link from the header

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -13,24 +13,6 @@ const Homepage = () => {
       </header>
 
       <main className="md:mb-36 mb-20">
-        <div className="text-xl text-wrap text-center gap-5 text-red-600 font-medium my-10">
-          <p>
-            This website is developed by Ayush Yadav
-            <br />
-            THIS IS NOT A REAL AGENCY WEBSITE <br />
-            this is just one of my projects
-          </p>
-          <a
-            href="https://ayush215mb.vercel.app/"
-            target="_blank"
-            className="hover:text-gray-400 text-white "
-          >
-            <p>
-              Click to get connected to me (the developer) to make awesome
-              websites like this
-            </p>
-          </a>
-        </div>
         <HeaderOne />
         <HeaderTwo />
         <HeaderFour />
